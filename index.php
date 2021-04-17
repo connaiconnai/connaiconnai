ed92df2f01ff8abee211584f3b5910abc01d2e7b
HaradaKazunari <dbxkazuxdb@gmail.com>
2021-04-18 08:42:15 +0900

    chenge site

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="自分を見つめ直すことができるプログラミング教室" name="description">
+    
+    <meta property="og:title" content="connai connai" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://haradakazunari.herokuapp.com/connaiconnai" />
+    <meta property="og:site_name" content="connai connai" />
+    <meta property="og:description" content="普段から着物を着て、イベントやプログラミング教室を企画・運営" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <title></title>
+  <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+  <header>
+    <div class="inline">
+      <div class="header-flex">
+        <div class="log">
+          <img src="./img/logo.png" alt="connai connai">
+        </div>
+        <div class="nav">
+          <div class="nav_btn"><a href="#about">About</a></div>
+          <div class="nav_btn"><a href="#service">Service</a></div>
+          <div class="nav_btn"><a href="#members">Members</a></div>
+          <div class="nav_btn"><a href="#contact">Contact</a></div>
+        </div>
+      </div><!-- flex -->
+    </div><!-- inline -->
+  </header>
+
+  <div class="rp_bg">
+    <div id="about">
+      <div class="inline">
+        <div class="title">
+          <h2>About</h2>
+        </div>
+        <div class="whbord">
+          <div class="flex">
+            <div class="icon"><img src="./img/logo.png" alt="connai connai"></div>
+            <div class="text">
+              <div class="bord_title">
+                <h3>connai connaiとは</h3>
+              </div>
+              <p>
+                パソコン初心者向けに、プログラミング家庭教師を開講しており、
+                エンジニアを目指すのではなく、「デジタルの世界」を知ったり、
+                パソコンの扱いに慣れたりすることで、自分にとって「楽しいこと」
+                「面白いこと」とは何なのかを考えるきっかけになって欲しいという想いで運営しています。
+              </p>
+              <a href="#conact">
+                <div class="button">
+                 無料体験を受講する 
+                </div>
+              </a>
+            </div><!-- text -->
+          </div><!-- flex -->
+        </div><!-- whbord -->
+      </div><!-- inline -->
+    </div><!-- about -->
+
+    <div id="service">
+      <div class="inline">
+        <div class="title">
+          <h2>Service</h2>
+        </div>
+        <div class="bord01">
+          <div class="whbord">
+            <div class="flex">
+              <div class="cell">
+                <div class="bord_title">
+                  <h3>HTML/CSS</h3>
+                </div>
+                <p>
+                  すでに公開されてあるサイトを模写して基礎を学び、その後、実際の制作の流れと同じようにサイト制作をおこないます。 また、自分で1から制作したサイトを公開するところまで行います。
+                </p>
+              </div><!-- cell -->
+              <div class="cell">
+                <div class="bord_title">
+                  <h3>PHP</h3>
+                </div>
+                <p>
+                  PHPは、Webサイトの中身（情報処理が必要なところ）を制作できる言語です。XAMPPというものを使用して開発します。 在庫管理システムや掲示板を制作し、HTML/CSS同様、サービスを公開するところまで行います。
+                </p>
+              </div><!-- cell -->
+              <div class="cell">
+                <div class="bord_title">
+                  <h3>Python</h3>
+                </div>
+                <p>
+                  プログラミングの基礎（順次・反復・分岐処理）について、じゃんけんゲームやブラックジャックを制作しながら学びます。 詳しく学びたい方は、さまざまなbotを制作しながら学ぶことができます。
+                </p>
+              </div><!-- cell -->
+            </div><!-- flex -->
+          </div><!-- whbord -->
+        </div><!-- bord01 -->
+
+
+        <div class="bord02">
+          <div class="flex">
+            <div class="whbord">
+              <div class="flex">
+                <div class="text">
+                  <div class="bord_title">
+                    <h3>受講について</h3>
+                  </div>
+                  <p>
+                    1回2時間の月2回~3回程度<br>
+                    各言語ごと12回を予定しております。<br> <br>
+
+                      受講する中でもっと続けたいというご希望があれば、12回以上続けることも可能です。
+                  </p>
+                </div><!-- text -->
+              </div><!-- flex -->
+            </div><!-- whbord -->
+            <div class="whbord">
+              <div class="flex">
+                <div class="text">
+                  <div class="bord_title">
+                    <h3>料金について</h3>
+                  </div>
+                  <p>
+                  <b>1回あたり8,000円</b>
+                  <br><br>
+                  複数人グループで受講の場合は、<br>料金を割引いたします。
+                  </p>
+                  <div class="al_flex" style="display:flex;">
+                    <p>
+                      2人グループ　<br>
+                      3人グループ　<br>
+                      4人グループ以上　
+                    </p>
+                    <p>
+                      → 7,000円/人<br>
+                      → 6,000円/人<br>
+                      → 5,000円/人
+                    </p>
+                  </div>
+                </div><!-- text -->
+              </div><!-- flex -->
+            </div><!-- whbord -->
+          </div><!-- flex -->
+        </div><!-- bord02 -->
+
+        <div class="bord03">
+          <div class="whbord">
+            <div class="flex">
+              <div class="text">
+                <div class="bord_title">
+                  <h3>この教室では</h3>
+                </div>
+                <p>
+                基本、対面で行います。<br>
+                （オンラインで行うことも可能）<br>
+                <!-- 「プログラミングの家庭教師」であるため、お家に伺います。<br> -->
+                パソコンについては、ご用意していただきます。
+                <br>
+                <br>
+                パソコン・プログラミング初心者のための教室であり、エンジニアを目指すものではありません。<br> プログラミングを学びたいけど難しそう、独学では難しいと感じている方を対象としています。<br>
+                <!-- そのため、「おばあちゃんにもわかりやすく」をモットーに開講しています。 -->
+                <br>
+                <br>
+
+                言語だけでなく、パソコン・ソフトウェア・ショートカットキーも学ぶことができ、<br>
+「作業の効率化」も身につけることができます。
+                </p>
+              </div><!-- text -->
+            </div><!-- flex -->
+          </div><!-- whbord -->
+        </div><!-- bord03 -->
+      </div><!-- inline -->
+    </div><!-- service -->
+  </div><!-- rp_bg -->
+
+  <div class="cl_bg">
+    <div id="members">
+      <div class="inline">
+        <div class="title">
+          <h2>Members</h2>
+        </div>
+        <div class="whbord">
+          <div class="flex">
+            <div class="text">
+              <div class="bord_title">
+                <h4>代表 : 原田 運也</h4>
+              </div>
+              <div class="photo sp photo1">
+                <img src="./img/harada.JPG" alt="Harada Kazunari">
+              </div>
+              <p>
+                久留米大学 商学部卒<br>
+                中学生の頃からプログラミングに興味を持つも挫折。<br>
+                商業高校進学後、そこでプログラミングを学ぶ。<br>
+                大学ではteam.csvというチームで、ボランティアとして
+                プログラミング教室を企画運営していた。<br>
+                NPO法人FOCUSで、キャリアサポーターとして
+                「就活生・社会人キャリア会@九州」を企画・運営していた。
+              </p>
+              <a href="https://haradakazunari.herokuapp.com">
+                <div class="button">
+                  Portfolio
+                </div>
+              </a>
+            </div><!-- text -->
+            <div class="photo pc photo2">
+              <img src="./img/harada.JPG" alt="Harada Kazunari">
+            </div>
+          </div><!-- flex -->
+        </div><!-- whbord -->
+      </div><!-- inline -->
+    </div><!-- members -->
+  </div><!-- cl_bg -->
+
+  <div class="bbg">
+    <div id="contact">
+      <div class="inline">
+        <div class="title">
+          <h2>Contact</h2>
+        </div>
+
+        <div class="bg_logo">
+          <img src="./img/blogo.png" alt="">
+        </div>
+        <div class="text">
+          <div class="text01">
+            <h5>Address</h5>
+            <p>
+            〒812-0893<br>
+            福岡市博多区那珂6丁目15
+            </p>
+          </div>
+          <div class="text02">
+            <h5>Email</h5>
+            <p>connaiconnai5252@gmail.com</p>
+          </div>
+          <div class="text03">
+            <!-- <h5>SNS</h5> -->
+            <!-- <div class="icons"> -->
+            <!--   <h4>SNS</h4> -->
+            <!--   <ul> -->
+            <!--     <li><a href="https://www.instagram.com/unya_gt/?hl=ja" target="_blank"><i class="fab fa-instagram"></i></a></li> -->
+            <!--     <li><a href="https://www.facebook.com/profile.php?id=100031652443406" target="_blank"><i class="fab fa-facebook-square"></i></a></li> -->
+
+            <!--     <li><a href="https://github.com/HaradaKazunari?tab=repositories" target="_blank"><i class="fab fa-github"></i></a></li> -->
+            <!--   </ul> -->
+            <!-- </div><!-1- /icons -1-> -->
+          </div>
+        </div>
+      </div><!-- inline -->
+    </div><!-- contact -->
+  </div><!-- bbg -->
+
+  <footer>
+    <p>&copy;copyright connai connai 2021</p>
+  </footer>
+  
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
