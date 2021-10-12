6404043addd0929ee42be0045e0789b278aa0c9b
HaradaKazunari <dbxkazuxdb@gmail.com>
2021-10-12 11:15:31 +0900

    wave animation

--- index.php
+++ index.php
@@ -77,6 +77,7 @@
         </div><!-- catch -->
       </div><!-- bg_white-->
       <div class="bg_animation">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg"><path id="wave" d=""/></svg>
       </div>
       <div class="bg_orange">
         <p class="about">
@@ -401,6 +402,11 @@
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<!-- wave -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js"></script>
+<script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/85188/jquery.wavify.js"></script>
+  <script src="js/wave.js"></script>
+<!-- wave -->
   <script src="js/hamberger.js"></script>
 </body>
 </html>
---
+++ js/wave.js
@@ -0,0 +1,7 @@
+$('#wave').wavify({
+  height: 60,
+  bones: 3,
+  amplitude: 40,
+  color: '#FEBF26',
+  speed: .25
+});
